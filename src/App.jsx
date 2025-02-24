494e3e3d1a12dc45a950d8711182f157762afa95
Kennnnn <huuthienandinh@gmail.com>
2025-02-25 03:58:19 +1000

    Update algorithm and comment code

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,44 @@
 
 import './App.css'
 import React from 'react'
-import QandA from './QandA'
-import {nanoid} from 'nanoid'
 import {decode} from 'html-entities'
 import Swal from 'sweetalert2'
 
 function App() {
 
-  // mot lan clicked la luu lai gia tri do, xong se check vs dap an tai do
   const [dataQuizz, setDataQuizz] = React.useState([])
   const [questions, setQuestions] =React.useState([])
   const [point, setPoint] = React.useState(0)
   const [clickedAnswer, setclickedAnswer] = React.useState(0)
 
-  const [answers, setAnswers] = React.useState(
-    [{
-    questions:"",
-    correct_answer:"",
-    all_answers:[],
-  }])
-
 
   React.useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=10")
     .then(res => res.json())
-    .then(data => setDataQuizz(data.results))   
+    .then(data => {
+      if (data.results){
+        setDataQuizz(data.results)
+      }
+      else{
+        console.error(" Invalid data format!", data)
+      }
+    })   
+    .catch(error => console.error("Failed to fetch data!", error))
 
   },[])
 
   function getAllQuestions(array) {
-     const mixAnswers = array.map(array => { 
-      const {correct_answer, incorrect_answers} = array;   
+     const mixAnswers = array.map(array => {                                                  ///mix and random answers
+      const {correct_answer, incorrect_answers} = array;                                  
       const all_answers = [...incorrect_answers, correct_answer]
       const randomAnswer = all_answers.slice().sort(() => Math.random() - 0.5)
       console.log(randomAnswer)
-      const all_answers_on = randomAnswer.map(answer =>{
+      const all_answers_on = randomAnswer.map(answer =>{                                       /// Add style for all answers
           return{
             answer: answer,
             style: false,
           }
       })
-      const indexCorrectAnswer = randomAnswer.findIndex(answer => answer == correct_answer)
-
-      console.log(correct_answer)
-
-      console.log(all_answers_on)
-
     return{
       ...array,
       correct_answer: correct_answer,
@@ -70,34 +62,27 @@ function App() {
     }, 0);
 
     setPoint(newPoints); 
-    Swal.fire("you got " + newPoints + " answers right");
+    Swal.fire("You got " + newPoints + " answers right");
 }
 
 
-  function changestyle(questionId, answerId){
-    const newQuestions = questions.map((question, qId) => 
-    qId === questionId
-      ? {
-          ...question,
-          all_answers: question.all_answers.map((answer, aId) => ({
-            ...answer,
-            style: !question.isClick && aId === answerId // Đổi style nếu chưa click
-              ? true
-              : question.isClick && question.clickedAnswer === aId // Reset nếu click lại
-              ? false
-              : answer.style, // Giữ nguyên style
-          })),
-          isClick: !question.isClick, // Đổi trạng thái câu hỏi
-          clickedAnswer: question.isClick ? null : answerId, // Cập nhật answer đã click
-        }
-      : question // Các câu hỏi khác giữ nguyên
-  );
-
-    setQuestions(newQuestions)
-    console.log(newQuestions)
-
+  function changeStyle(questionId, answerId) {
+    setQuestions(prevQuestions =>
+      prevQuestions.map((question, qId) =>
+        qId === questionId
+          ? {
+              ...question,
+              all_answers: question.all_answers.map((answer, aId) => ({
+                ...answer,
+                style: aId === answerId, // Only change style if answer is chosen from user
+              })),
+              isClick: true, // Question is clicked by user
+              clickedAnswer: answerId,
+            }
+          : question
+      )
+    );
   }
-
 
     React.useEffect(() => {
       const clickedAnswer = questions.filter(question => question.isClick).length;
@@ -105,14 +90,11 @@ function App() {
     }, [questions]);
 
 
-
-
-
   function startGame(){
     const newQuestions = getAllQuestions(dataQuizz)
     setQuestions(newQuestions) 
-
   }
+
 
 
   const QuestionElement = questions.map((question, questionId) =>
@@ -124,12 +106,11 @@ function App() {
         </h2>
 
         {question.all_answers.map((answer, answerId) =>
-            <button onClick={()=>changestyle(questionId, answerId)} className={answer.style === false ? 'boxAnswers' : 'boxAnswersClicked'} key={answer.answer}> 
+            <button onClick={()=>changeStyle(questionId, answerId)} className={answer.style === false ? 'boxAnswers' : 'boxAnswersClicked'} key={answer.answer}> 
               {decode(answer.answer)} 
             </button>)}
 
     </div>)
-
 
 
   return (
